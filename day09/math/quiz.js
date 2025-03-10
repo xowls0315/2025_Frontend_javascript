7907d8ad57effb9c8efa56812b6ac99d994175ea
t.jin_01 <155832601+xowls0315@users.noreply.github.com>
2025-03-10 21:22:44 +0900

    Add files via upload

--- day09/math/quiz.js
+++ day09/math/quiz.js
@@ -1,24 +1,24 @@
 // 랜덤한 배경색상을 가진 버튼을 10개 만들기
-// 컨텐츠는 "버튼"
-
+// 컨텐츠는 버튼
 const color = [..."1234567890abcdef"];
-
-// min~max 정수 랜덤하게 뽑는 함수
 const getRandom = (min, max) => {
-  return Math.floor(Math.random() * (max, min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-const getColorRandom = () =>
-  "#" + Array(6).fill(color[getRandom(0, color.length)]).join("");
-// color[getRandom(0, color.length)];
+const getColorRandom = () => {
+  return (
+    "#" +
+    Array(6)
+      .fill(0)
+      .map((v) => color[getRandom(0, color.length)])
+      .join("")
+  );
+};
 
 Array(10)
   .fill(0)
-  .forEach(() => {
-    const tag = document.createElement("button");
-
-    tag.style.backgroundColor = getColorRandom();
-    tag.innerHTML = "버튼";
-
-    document.body.appendChild(tag);
+  .forEach((x) => {
+    const btn = document.createElement("button");
+    btn.innerText = "버튼";
+    btn.style.backgroundColor = getColorRandom();
+    document.body.appendChild(btn);
   });
