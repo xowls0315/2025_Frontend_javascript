58a2a3bdb70d2c1fb5dd710efae96af43e3a9b66
t.jin_01 <155832601+xowls0315@users.noreply.github.com>
2025-03-21 16:00:42 +0900

    Add files via upload

--- socar_my/javascript/index.js
+++ socar_my/javascript/index.js
@@ -3,8 +3,8 @@ const anytime = document.querySelector(".anytime");
 const wantplace = document.querySelector(".wantplace");
 const needtime = document.querySelector(".needtime");
 const manycar = document.querySelector(".manycar");
-
-const header = document.querySelector("header");
+const skewer = document.querySelector(".startsocar__skewer");
+const download = document.querySelector(".download");
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -17,15 +17,52 @@ const observer = new IntersectionObserver(
   },
   { threshold: 0.5 }
 );
-
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("fade-out-right");
+        entry.target.classList.add("fade-in-left");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
 observer.observe(heroContents);
-observer.observe(anytime);
-observer.observe(wantplace);
-observer.observe(needtime);
-observer.observe(manycar);
+observer2.observe(anytime);
+observer2.observe(wantplace);
+observer2.observe(needtime);
+observer2.observe(manycar);
+observer2.observe(skewer);
+observer2.observe(download);
+
+const header = document.querySelector("header");
+const logo = document.querySelector("#logo");
+const upbar = document.querySelector(".upBar");
+const downbar = document.querySelector(".downBar");
 
 window.addEventListener("scroll", () => {
-  header.scrollY != 0
-    ? (header.style.backgroundColor = "white")
-    : (header.style.backgroundColor = "transparent");
+  const { scrollY } = window;
+  if (scrollY > 0) {
+    logo.src = "https://www.socar.kr/images/logo-blue.svg";
+    header.classList.add("act");
+    upbar.classList.add("blue");
+    downbar.classList.add("blue");
+    upbar.classList.remove("white");
+    downbar.classList.remove("white");
+  } else {
+    logo.src = "https://www.socar.kr/images/logo-w.svg";
+    header.classList.remove("act");
+    upbar.classList.add("white");
+    downbar.classList.add("white");
+    upbar.classList.remove("blue");
+    downbar.classList.remove("blue");
+  }
+});
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  upbar.classList.toggle("upbarX");
+  downbar.classList.toggle("downbarX");
 });
